a9100959bb7b6241376f095fdf4485003d7c40a8
Liza-liza <lizz4mail@gmail.com>
2018-02-25 19:52:29 +0300

    Update

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script receives a list of URLs from text file and checks them up for 2 points: 
+1) the server responds for request by status 200
+2) domain name is paid for a month or longer
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python3 check_sites_health.py <filename>
+```
+where <filename> is the file with URLs list for testing.
+
+Note, the script needs additional python modules to work. It would be installed by command:
+
+```bash
+
+$ pip3 install -r requirements.txt
+```
+
+# Output example
+
+```
+$ python3 check_sites_health.py url_list
+
+https://pythonworld.ru:
+ request HTTP 200: True,
+ Domain Name paid for 1 month and more: True
+
+https://habrahabr.ru:
+ request HTTP 200: True,
+ Domain Name paid for 1 month and more: True
+
+http://naturaliste.ru:
+ request HTTP 200: True,
+ Domain Name paid for 1 month and more: True
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,64 @@
-def load_urls4check(path):
-    pass
+import sys
+import requests
+from whois import whois
+from datetime import datetime
+
+
+def get_urls4check(filepath):
+    with open(filepath, 'r') as file:
+        for url in file:
+            if url != '\n':
+                yield url.strip('\n ')
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url)
+        if response.status_code == requests.codes.ok:
+            return True
+    except requests.exceptions.InvalidSchema:
+        return False
+    except requests.exceptions.ConnectionError:
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def check_domain_expiration_date(domain_name):
+    response = whois(domain_name)
+    if isinstance(response.expiration_date, datetime):
+            expiration_date = response.expiration_date
+    elif isinstance(response.expiration_date, list):
+            expiration_date = response.expiration_date[0]
+    if (expiration_date - datetime.now()).days > 30:
+            return True
+
+
+def print_result(result_dict):
+    for url, status in result_dict.items():
+        print('\n{}:\n request HTTP 200: {},\n '
+              'Domain Name paid for 1 month and more: {}'
+              .format(url, status[respond_index], status[date_index]))
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        respond_index = 0
+        date_index = 1
+
+        result_dict = {url: [False, False] for url
+                       in get_urls4check(sys.argv[1])}
+        for url in result_dict.keys():
+            if is_server_respond_with_200(url):
+                result_dict[url][respond_index] = \
+                    is_server_respond_with_200(url)
+                result_dict[url][date_index] = \
+                    check_domain_expiration_date(url)
+            else:
+                result_dict[url]= ['Invalid URL', 'Invalid URL']
+        print_result(result_dict)
+    except IndexError:
+        print("Launch: $python3 check_site_health.py <path_to_file>"
+              "where <pathe_to_file> is a file with list of urls for checking")
+    except FileNotFoundError:
+        print('File {} not found'.format(sys.argv[1]))
+
+
