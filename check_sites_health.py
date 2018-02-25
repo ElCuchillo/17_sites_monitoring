3b60b9eb628f631621cb898b5c579e0fbddd820b
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-02-25 23:30:26 +0300

    functions  simplifyed

--- check_sites_health.py
+++ check_sites_health.py
@@ -25,36 +25,37 @@ def is_server_respond_with_200(url):
 def check_domain_expiration_date(domain_name):
     response = whois(domain_name)
     if isinstance(response.expiration_date, datetime):
-            expiration_date = response.expiration_date
+        expiration_date = response.expiration_date
     elif isinstance(response.expiration_date, list):
-            expiration_date = response.expiration_date[0]
+        expiration_date = response.expiration_date[0]
     if (expiration_date - datetime.now()).days > 30:
             return True
 
 
-def print_result(result_dict):
-    for url, status in result_dict.items():
+def check_url(url, results_dict):
+    if is_server_respond_with_200(url):
+        results_dict[url][respond] = is_server_respond_with_200(url)
+        results_dict[url][date] = check_domain_expiration_date(url)
+    else:
+        results_dict[url] = ['Invalid URL', 'Invalid URL']
+
+
+def print_result(results_dict):
+    for url, status in results_dict.items():
         print('\n{}:\n request HTTP 200: {},\n '
               'Domain Name paid for 1 month and more: {}'
-              .format(url, status[respond_index], status[date_index]))
+              .format(url, status[respond], status[date]))
 
 
 if __name__ == '__main__':
     try:
-        respond_index = 0
-        date_index = 1
-
-        result_dict = {url: [False, False] for url
+        respond = 0
+        date = 1
+        results_dict = {url: [False, False] for url
                        in get_urls4check(sys.argv[1])}
-        for url in result_dict.keys():
-            if is_server_respond_with_200(url):
-                result_dict[url][respond_index] = \
-                    is_server_respond_with_200(url)
-                result_dict[url][date_index] = \
-                    check_domain_expiration_date(url)
-            else:
-                result_dict[url]= ['Invalid URL', 'Invalid URL']
-        print_result(result_dict)
+        for url in results_dict.keys():
+            check_url(url, results_dict)
+        print_result(results_dict)
     except IndexError:
         print("Launch: $python3 check_site_health.py <path_to_file>"
               "where <pathe_to_file> is a file with list of urls for checking")
