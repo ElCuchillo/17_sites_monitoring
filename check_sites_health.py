7e6225f28983efc41e63f593eaa36b32fe75538e
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-03-07 22:53:04 +0300

    excess if-else deleted

--- check_sites_health.py
+++ check_sites_health.py
@@ -14,11 +14,8 @@ def get_urls4check(filepath):
 def is_server_respond_ok(url):
     try:
         response = requests.get(url)
-        if response.status_code == requests.codes.ok:
-            return True
-    except requests.exceptions.InvalidSchema:
-        return False
-    except requests.exceptions.ConnectionError:
+        return response.ok
+    except Exception:
         return False
 
 
@@ -30,10 +27,7 @@ def check_domain_expiration_date(domain_name, paid_period=30):
         expiration_date = response.expiration_date
     elif isinstance(response.expiration_date, list):
         expiration_date = response.expiration_date[0]
-    if (expiration_date - datetime.now()).days > paid_period:
-        return True
-    else:
-        return False
+    return ((expiration_date - datetime.now()).days > paid_period)
 
 
 def check_url(url):
@@ -41,20 +35,20 @@ def check_url(url):
     if server_ok:
         return [server_ok, check_domain_expiration_date(url)]
     else:
-        return ['Invalid URL', 'Invalid URL']
+        return ['Invalid URL', check_domain_expiration_date(url)]
 
 
-def print_result(results_dict):
+def print_result(results_dict, time_period=30):
+    respond = 0
+    date = 1
     for url, status in results_dict.items():
         print('\n{}:\n request HTTP 200: {},\n '
-              'Domain Name paid for 1 month and more: {}'
-              .format(url, status[respond], status[date]))
+              'Domain Name paid for {} days and more: {}'
+              .format(url, status[respond], time_period, status[date]))
 
 
 if __name__ == '__main__':
     try:
-        respond = 0
-        date = 1
         results_dict = {}
         for url in get_urls4check(sys.argv[1]):
             results_dict[url] = check_url(url)
@@ -64,5 +58,3 @@ if __name__ == '__main__':
               'where <pathe_to_file> is a file with list of urls for checking')
     except FileNotFoundError:
         print('File {} not found'.format(sys.argv[1]))
-
-
